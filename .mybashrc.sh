00656b2369c31cfba77d1ed39908f18ba63b7b7d
qighp@arch.(none) <qighp@arch.(none)>
2024-08-28 14:04:37 +0200

    save open and load dir

--- .mybashrc.sh
+++ .mybashrc.sh
@@ -5,3 +5,18 @@ export PAGER='less'
 alias nv="nvim"
 set _JAVA_AWT_WM_NONREPARENTING=1
 export PS1='$(RETVAL=$?; [ $RETVAL -ne 0 ] && echo "\[\033[0;31m\][$RETVAL]")[\u@\h \W]\$ \[\033[0m\]'
+
+# open last saved dir
+if [[ -f "$HOME/.predir" ]]; then
+	cd $(tail -n1 "$HOME/.predir") || return
+fi
+
+# save dir
+sd(){
+	pwd >> "$HOME/.predir"
+}
+
+# open dir
+od(){
+	[[ -f "$HOME/.predir" ]] && cd $(cat "$HOME/.predir" | fzf)
+}
